af52d960247771055444326e713ce15432825653
Aleksandr2612 <101992736+Aleksandr2612@users.noreply.github.com>
2022-05-10 21:30:02 +0300

    Add files via upload

---
+++ js-01-houmwork/js/script.js
@@ -0,0 +1,134 @@
+// console.log("Hello1");
+// console.log("Hello2");
+// console.log("Hello3");
+// console.log("Hello4");
+
+
+// let message;
+
+// message = 'Hello World'; // сохранить строку
+// console.log(message);
+
+// message = 200;
+// console.log(message);
+// const myBirthday = '18.04.1982';
+// console.log(myBirthday);
+// let str = "one";
+// let str2 = "two";
+// let str3 = 'string 3 ${str}';
+// // console.log(str, str2, str3);
+// console.log(str3);
+// console.log(str3);
+// console.log(str3);
+
+// let bool = true;
+// let boolStr = "true"
+// console.log(typeof(boolStr), "tjhgjg;jh; boolStr");
+
+// let boolStrType = typeof(boolStr);
+// alert("Man4ester");
+// let userName = prompt("What is your name", "Ivan")
+// console.log('Hi, ${userName}');
+
+// let isBoss = confirm("Ты здесь главный?");
+
+// alert(isBoss); // true, если нажата OK
+
+// let value = true;
+// console.log(value);
+
+// value = String(value); // теперь value это строка "true"
+// console.log(value);
+// // alert(typeof value); // string
+
+// let math = "6" / "2";
+// console.log(math);
+// let mathg = "kjg" / "2";
+// console.log(mathg);
+
+alert("Привет! Это работа Клепца Александра Николаевича.");
+
+let planetName = 'Mars';
+console.log(planetName);
+let myAge = '40 лет';
+console.log(myAge);
+//вывод значения переменной в консоль
+let autoBreak;
+autoBreak = 'BMW';
+console.log(autoBreak);
+let colorHeader = 'jellow';
+console.log(colorHeader);
+
+
+
+let myName = 'Александр',
+    surname = 'Клепец';
+const MYBIRTHDAY = '26.12.1981'
+
+alert(`Привет, меня зовут ${myName} ${surname}! Мне ${myAge}`);
+console.log(`Привет, меня зовут ${myName} ${surname}! Мне ${myAge}`);
+console.log(`Дата моего рождения: ${MYBIRTHDAY}`);
+
+let nameChecked = prompt('Какое имя у проверяющего мою работу?', 'Игорь');
+// alert(nameChecked);
+alert(`Мою работу проверяет ${nameChecked}`);
+
+// let name = prompt("Ваше имя?", "");
+// alert(name);
+
+let str = "String";
+alert(typeof str);
+console.log(typeof str);
+
+let num = 2000;
+alert(typeof num);
+console.log(typeof num);
+
+let testSkiped = false;
+alert(typeof testSkiped);
+console.log(typeof testSkiped);
+
+let month = undefined;
+alert(typeof month);
+console.log(typeof month);
+
+
+let data = null;
+alert(typeof data);
+console.log(typeof data);
+
+let result = 0 / 0;
+alert(typeof result);
+console.log(typeof result);
+
+let newStr = "37";
+console.log(typeof result);
+let numberr = Number(newStr);
+console.log(typeof newStr);
+
+// let age = Number("Любая строка вместо числа");
+
+// alert(age);
+newStr = Boolean(newStr);
+console.log(typeof newStr);
+
+let newValue = true;
+console.log(typeof newValue);
+
+newValue = String(newValue);
+console.log(typeof newValue);
+
+let numer = Number(newValue);
+console.log(typeof numer);
+
+let newNum = 2007;
+console.log(typeof newNum);
+newNum = String(newNum);
+console.log(typeof newNum);
+newNum = Boolean(newNum);
+console.log(typeof newNum);
+
+let newname = 'Ivan';
+console.log(newname);
+let surName = newname;
+console.log(surName);
